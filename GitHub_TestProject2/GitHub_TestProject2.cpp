0076d0d5db7c4e323217ce5ee8c2e02571a15f50
GitHubVS2013PH <phayter1@gmail.com>
2015-10-16 15:30:00 -0700

    Project2 restart; first commit

---
+++ GitHub_TestProject2/GitHub_TestProject2.cpp
@@ -0,0 +1,11 @@
+// GitHub_Test_Project2.cpp : Defines the entry point for the console application.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   cout << "Hello World. Testing GitHub with a new Project (Project #2)!\n";
+   return 0;
+}
